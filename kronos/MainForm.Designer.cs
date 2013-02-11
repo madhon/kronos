f8b03dc4a5b0341c061520c0fd3ea8c4ecfdf8bd
Madhon <madhon@madhon.co.uk>
2013-02-11 10:04:18 +0000

    bitbucket move

---
+++ kronos/MainForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Kronos
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtActLog = new System.Windows.Forms.TextBox();
+            this.txtAct = new System.Windows.Forms.TextBox();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.cmdAddAct = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(369, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // txtActLog
+            // 
+            this.txtActLog.BackColor = System.Drawing.SystemColors.Window;
+            this.txtActLog.Location = new System.Drawing.Point(12, 27);
+            this.txtActLog.Multiline = true;
+            this.txtActLog.Name = "txtActLog";
+            this.txtActLog.ReadOnly = true;
+            this.txtActLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtActLog.Size = new System.Drawing.Size(345, 310);
+            this.txtActLog.TabIndex = 1;
+            // 
+            // txtAct
+            // 
+            this.txtAct.Location = new System.Drawing.Point(91, 345);
+            this.txtAct.Name = "txtAct";
+            this.txtAct.Size = new System.Drawing.Size(185, 20);
+            this.txtAct.TabIndex = 2;
+            this.txtAct.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtAct_KeyDown);
+            // 
+            // lblTime
+            // 
+            this.lblTime.Location = new System.Drawing.Point(9, 348);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(76, 13);
+            this.lblTime.TabIndex = 3;
+            this.lblTime.Text = "1h 55m";
+            // 
+            // cmdAddAct
+            // 
+            this.cmdAddAct.Location = new System.Drawing.Point(282, 343);
+            this.cmdAddAct.Name = "cmdAddAct";
+            this.cmdAddAct.Size = new System.Drawing.Size(75, 23);
+            this.cmdAddAct.TabIndex = 4;
+            this.cmdAddAct.Text = "&Add";
+            this.cmdAddAct.UseVisualStyleBackColor = true;
+            this.cmdAddAct.Click += new System.EventHandler(this.OnAddActivity);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 370);
+            this.Controls.Add(this.cmdAddAct);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.txtAct);
+            this.Controls.Add(this.txtActLog);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Kronos";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtActLog;
+        private System.Windows.Forms.TextBox txtAct;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button cmdAddAct;
+    }
+}
+
---
+++ kronos/MainForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Kronos
+{
+    public partial class MainForm : Form
+    {
+        private DateTime LastActTime;
+        private TimeSpan totalDuration;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            lblTime.Text = "";
+            LastActTime = DateTime.Now;
+        }
+
+        private void OnAddActivity(object sender, EventArgs e)
+        {
+            if (txtAct.Text.ToUpper() == "ARRIVED")
+            {
+                LastActTime = DateTime.Now;
+                TimeSpan arrived = LastActTime - LastActTime;
+                AddLineToLog(arrived, LastActTime, LastActTime, "Arrived");
+                return;
+            }
+
+            DateTime currTime = DateTime.Now;
+            TimeSpan activityDuration = currTime - LastActTime;
+            totalDuration += activityDuration;
+
+            AddLineToLog(activityDuration, LastActTime, currTime, txtAct.Text);
+
+            if (!txtAct.Text.EndsWith("**"))
+                UpdateTotalDuration();
+                        
+            LastActTime = currTime;
+
+            txtAct.Text = "";
+        }
+
+        private void AddLineToLog(TimeSpan duration, DateTime startTime, DateTime endTime, string activity)
+        {
+            string durationSring = String.Format("{0} h {1} min", duration.Hours, duration.Minutes);
+            string message = String.Format("{0}\t\t({1} - {2})\t{3}{4}", durationSring, startTime.ToShortTimeString(), endTime.ToShortTimeString(), activity, Environment.NewLine);
+            txtActLog.Text += message;
+        }
+
+        private void UpdateTotalDuration()
+        {
+            lblTime.Text = String.Format("{0} h {1} min", totalDuration.Hours, totalDuration.Minutes);
+        }
+
+        private void txtAct_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.KeyCode == Keys.Enter))
+                OnAddActivity(sender, e);
+        }
+
+    }
+}
